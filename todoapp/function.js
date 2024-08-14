5ca83320d32a63d296183e09856ecadb4288ae4b
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-08-14 11:19:10 +0200

    todo exemple

--- todoapp/function.js
+++ todoapp/function.js
@@ -4,15 +4,5 @@ function houlou(arg) {
 const arrow = (arg) => {
   return arg;
 };
-const tasks = [
-  { name: "vaisselle", state: false },
-  {
-    name: "sport",
-    state: true,
-  },
-  {
-    name: "super skills",
-    state: false,
-  },
-];
+
 export { houlou, arrow, tasks };
--- todoapp/script.js
+++ todoapp/script.js
@@ -1,39 +1,61 @@
-import { houlou, arrow, tasks } from "./function.js";
+let tasks = [
+  { name: "vaisselle", state: false },
+  {
+    name: "sport",
+    state: true,
+  },
+  {
+    name: "super skills",
+    state: false,
+  },
+];
 
-const singleTask = tasks.find((task) => task.name == "sport");
-console.log(singleTask);
-const taskindex = tasks.findIndex((task) => task.name == "suer skills");
-console.log(taskindex);
-const done = tasks.filter((v) => v.state == true);
-console.log(done);
-const notDone = tasks.filter((v) => v.state == false);
-console.log(notDone);
-const urgent = tasks.map((v, i) => {
-  if (v.name.length > 5) {
-    return { ...v, urgent: true };
-  } else {
-    return { ...v, urgent: false };
-  }
-});
-console.log(urgent);
-const body = document.querySelector("body");
+const form = document.querySelector("form");
+const addTask = document.querySelector("#add");
 const tasksWrap = document.querySelector("#tasks-wrap");
-tasksWrap.innerHTML = tasks.map((v) => {
-  return `<div class="single-task flex between">
-              <div class="flex">
-                <form class="checked">
-                  <input type="checkbox" class="state" />
-                </form>
-                <p class=${v.state == true ? "lined" : "notlined"}>${v.name}</p>
-              </div>
-              <button class="delete">
-                <img src="./images/icon-cross.svg" alt="icon-cross" />
-              </button>
-            </div>`;
+
+const display = () => {
+  tasksWrap.innerHTML = tasks.map((v) => {
+    return `<div class="single-task flex between">
+                <div class="flex">
+                  <form class="checked">
+                    <input type="checkbox" ${
+                      v.state == true ? "checked" : ""
+                    } class="state" />
+                  </form>
+                  <p class=${v.state == true ? "lined" : "notlined"}>${
+      v.name
+    }</p>
+                </div>
+                <button class="delete">
+                  <img src="./images/icon-cross.svg" alt="icon-cross" />
+                </button>
+              </div>`;
+  });
+  const state = document.querySelectorAll(".state");
+  state.forEach((v, i) => {
+    v.addEventListener("change", () => {
+      tasks[i].state = v.checked;
+      console.log(tasks);
+      display();
+    });
+  });
+  const deleteBtn = document.querySelectorAll(".delete");
+  deleteBtn.forEach((v, i) => {
+    v.addEventListener("click", () => {
+      const filtered = tasks.filter((item) => item !== tasks[i]);
+      tasks = [...filtered];
+      //  console.log(filtered);
+      display();
+    });
+  });
+};
+display();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = addTask.value;
+  tasks.push({ name, state: false });
+  addTask.value = "";
+  console.log(tasks);
+  display();
 });
-if (9 > 5) {
-  console.log("hello");
-} else {
-  console.log("buy");
-}
-9 > 5 ? console.log("noope") : console.log("hohoh");
