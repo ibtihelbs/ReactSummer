0d341d49f8c7f47f0f44ec0ad5667ce7632be738
ibtihelbs <ibtihel.bensalah@outlook.fr>
2024-08-20 11:36:26 +0200

    state

--- portfolio/src/componants/Nav.jsx
+++ portfolio/src/componants/Nav.jsx
@@ -1,5 +1,40 @@
+import reactlogo from "../assets/react.svg";
+import { useState } from "react";
 const Nav = () => {
-  return <div>Nav</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const navLinks = [
+    { name: "Home", link: "#" },
+    { name: "About", link: "#about" },
+    { name: "Portfolio", link: "#portfolio" },
+    { name: "Contact", link: "#contact" },
+  ];
+  console.log(isOpen);
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "0 2rem",
+      }}
+    >
+      <img src="/vite.svg" alt="logo" />
+      <button
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      >
+        <img src={reactlogo} alt="react logo" id="menu" />
+      </button>
+      <ul className={`nav ${isOpen == true ? "open" : ""}`}>
+        {navLinks.map((v, i) => (
+          <li key={i}>
+            <a href={v.link}> {v.name} </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
